79cf5e120321ae0fcca01092bd11c1aa7175761c
Hybrid <hybrid_46_2@hotmail.com>
2023-12-20 15:09:08 +0100

    [Mod] Perlin based map generation

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -19,12 +19,23 @@ public class MapGenerator : MonoBehaviour
         {
             for (int x = 0; x < tiles; x++)
             {
-                tileMap.SetTile(new Vector3Int(x,y), ruleTiles[0]);
+                Vector3Int position = new Vector3Int(x, y);
+                float noiseValue = GetPerlin(position, 20f, Vector2.zero);
+
+                if (noiseValue > 0.5f) tileMap.SetTile(position, ruleTiles[0]);
             }
         }
 
         tileMap.SetTile(new Vector3Int(2, 2), null);
         tileMap.SetTile(new Vector3Int(6, 6), null);
         tileMap.SetTile(new Vector3Int(6, 7), null);
+    }
+
+    private float GetPerlin(Vector3Int position, float scale, Vector2 offset)
+    {
+        float xCoord = (float)position.x / mapSize * scale + offset.x;
+        float zCoord = (float)position.y / mapSize * scale + offset.y;
+
+        return Mathf.PerlinNoise(xCoord, zCoord);
     }
 }
