743fcbc9ccbe97a63356e3234b0b1b32f2a9b0c5
Hybrid <hybrid_46_2@hotmail.com>
2023-12-24 11:27:52 +0100

    Added ore generation, the first ore -> copper

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using Debug = UnityEngine.Debug;
+using Random = UnityEngine.Random;
 
 public class MapGenerator : MonoBehaviour
 {
@@ -13,13 +15,58 @@ public class MapGenerator : MonoBehaviour
     [Range(1, 10)][Tooltip("Iterations for Diamond Square Algo -> Higher values makes the terrain more natural.")] public int dsqIterations = 5;
     [Range(0, 10)][Tooltip("Iteration count for smoothing the terrain.")] public int smoothingIterations = 1;
 
-    public Tilemap tileMap;
+    public Tilemap terrainTileMap;
+    public Tilemap propsRocksTileMap;
     public List<RuleTile> ruleTiles;
 
     [Tooltip("Seeds for map generation. dsqIterations count and seed count must be qual!")] public List<Vector2> seeds;
 
-    public float[,] heightMap { get; private set; }
     [SerializeField] private Texture2D mapTexture;
+
+    [Flags]
+    public enum TileHeightType : byte
+    {
+        None = 0,
+        Ground = 1 << 0,
+        Water = 1 << 1,
+        Cliff = 1 << 2,
+    }
+
+    public float[,] heightMap { get; private set; }
+
+    public TileHeightType[,] heightMapType { get; private set; }
+
+    public enum MapType { Summer, Winter, DeepOcean }
+
+    [Serializable]
+    public struct MapSettings
+    {
+        public MapType type;
+        [Range(0.0f, 0.9f)] public float oreFieldSize;
+        public float oreFieldSpacing;
+        [Range(0.0f, 1.0f)] public float cliffContinuity;
+    }
+
+    public MapSettings mapSettings;
+
+    public enum OreType
+    {
+        Copper = 0,
+        Sand = 1,
+        Water = 2
+    }
+
+    public OreType[,] oreMap;
+
+    [Serializable]
+    public struct Ore
+    {
+        public OreType type;
+        public RuleTile tile;
+    }
+
+    public List<Ore> ores;
+    public Dictionary<OreType, RuleTile> oresD;
 
     void Start()
     {
@@ -27,6 +74,7 @@ public class MapGenerator : MonoBehaviour
         stopwatch.Start();
         //TODO -> job
         Generate();
+        InitializeOres();
         stopwatch.Stop();
         Debug.Log($"Generate time -> {stopwatch.ElapsedMilliseconds} ms");
         stopwatch.Reset();
@@ -42,6 +90,12 @@ public class MapGenerator : MonoBehaviour
         stopwatch.Stop();
         Debug.Log($"GenerateMapTexture time -> {stopwatch.ElapsedMilliseconds} ms");
         stopwatch.Reset();
+
+        stopwatch.Start();
+        GenerateOresAndCliffs();
+        stopwatch.Stop();
+        Debug.Log($"GenerateOresAndCliffs time -> {stopwatch.ElapsedMilliseconds} ms");
+        stopwatch.Reset();
     }
 
     public float GetHeight(int x, int y) => heightMap[x, y];
@@ -55,7 +109,7 @@ public class MapGenerator : MonoBehaviour
         DiamondSquareGenerator dsqg = new DiamondSquareGenerator(mapSize, terrainRoughness, averageHeight, terrainMicroRoughness, seeds.ToArray());
         heightMap = dsqg.GenerateTerrain(dsqIterations);
 
-        tileMap.enabled = false;
+        terrainTileMap.enabled = false;
 
         for (int y = 0; y < mapSize; y++)
         {
@@ -64,13 +118,13 @@ public class MapGenerator : MonoBehaviour
                 Vector3Int position = new Vector3Int(x, y);
 
                 if (heightMap[x, y] > 0.5f)
-                    tileMap.SetTile(position, ruleTiles[0]);
+                    terrainTileMap.SetTile(position, ruleTiles[0]);
                 else
-                    tileMap.SetTile(position, ruleTiles[1]);
+                    terrainTileMap.SetTile(position, ruleTiles[1]);
             }
         }
 
-        tileMap.enabled = true;
+        terrainTileMap.enabled = true;
     }
 
     private void GenerateMapTexture()
@@ -114,4 +168,95 @@ public class MapGenerator : MonoBehaviour
         }
     }
 
+    private void InitializeOres()
+    {
+        oreMap = new OreType[mapSize, mapSize];
+        oresD = new Dictionary<OreType, RuleTile>(ores.Count);
+
+        ores.ForEach(ore => oresD.Add(ore.type, ore.tile));
+    }
+
+    private void GenerateOresAndCliffs()
+    {
+        List<Vector2> poissonPoints = GeneratePoissonPoints(5.0f, 20);
+
+        //generate ores and cliffs
+        foreach (Vector2 point in poissonPoints)
+        {
+            Vector2Int roundedPoint = Vector2Int.RoundToInt(point);
+
+            //TODO floodfill ores and make cliffs
+
+            //testing
+            OreType oreType = (OreType)Random.Range(1, ores.Count) - 1;
+            Debug.Log(oreType.ToString());
+            oreMap[roundedPoint.x, roundedPoint.y] = oreType;
+
+            propsRocksTileMap.SetTile(new Vector3Int(roundedPoint.x, roundedPoint.y), GetOreRuleTile(oreType));
+        }
+    }
+
+    private RuleTile GetOreRuleTile(OreType oreType) => oresD[oreType];
+
+    private List<Vector2> GeneratePoissonPoints(float minDistance, int maxAttempts)
+    {
+        Vector2 bounds = new Vector2(mapSize - 1, mapSize - 1);
+        List<Vector2> points = new List<Vector2>();
+        float cellSize = minDistance / Mathf.Sqrt(2);
+        int[,] grid = new int[Mathf.CeilToInt(bounds.x / cellSize), Mathf.CeilToInt(bounds.y / cellSize)];
+        List<Vector2> activeList = new List<Vector2>();
+
+        Vector2 firstPoint = new Vector2(Random.Range(0, bounds.x), Random.Range(0, bounds.y));
+        points.Add(firstPoint);
+        activeList.Add(firstPoint);
+        grid[Mathf.FloorToInt(firstPoint.x / cellSize), Mathf.FloorToInt(firstPoint.y / cellSize)] = points.Count;
+
+        while (activeList.Count > 0)
+        {
+            int randomIndex = Random.Range(0, activeList.Count);
+            Vector2 point = activeList[randomIndex];
+            bool foundPoint = false;
+
+            for (int i = 0; i < maxAttempts; i++)
+            {
+                float angle = Random.Range(0, Mathf.PI * 2);
+                float radius = Random.Range(minDistance, minDistance * 2);
+                Vector2 newPoint = new Vector2(point.x + radius * Mathf.Cos(angle), point.y + radius * Mathf.Sin(angle));
+
+                if (newPoint.x >= 0 && newPoint.x < bounds.x && newPoint.y >= 0 && newPoint.y < bounds.y)
+                {
+                    int cellX = Mathf.FloorToInt(newPoint.x / cellSize);
+                    int cellY = Mathf.FloorToInt(newPoint.y / cellSize);
+                    bool canPlace = true;
+
+                    for (int x = Mathf.Max(0, cellX - 2); x < Mathf.Min(grid.GetLength(0), cellX + 3); x++)
+                    {
+                        for (int y = Mathf.Max(0, cellY - 2); y < Mathf.Min(grid.GetLength(1), cellY + 3); y++)
+                        {
+                            if (grid[x, y] > 0 && Vector2.Distance(points[grid[x, y] - 1], newPoint) < minDistance)
+                            {
+                                canPlace = false;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (canPlace)
+                    {
+                        points.Add(newPoint);
+                        activeList.Add(newPoint);
+                        grid[cellX, cellY] = points.Count;
+                        foundPoint = true;
+                    }
+                }
+            }
+
+            if (!foundPoint)
+            {
+                activeList.RemoveAt(randomIndex);
+            }
+        }
+
+        return points;
+    }
 }
